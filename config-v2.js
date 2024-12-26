27e3add89330f517886d85645006f382369ae0e3
nivankadilshan <109707385+nivankadilshan@users.noreply.github.com>
2024-12-26 09:16:07 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
 const GITHUB_AUTH_TOKEN = "null"
 const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const SESSION_ID = "PRABATH-MD~hnVXTLhR#Ih2O2eyY9fm9wfSQHQB1_-wBYkUVJulcfRq8DG_mu4A"
 
 
 module.exports = {
